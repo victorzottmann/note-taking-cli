2cf08d647a0fa1f7c7b2f8b21f4ed7f0d64c4f3e
Victor Zottmann <zottmann.victor@gmail.com>
2023-11-19 22:09:11 +1100

    add inital structure of commands using `yargs`

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Node.js Note-Taking CLI
 
-To create a CLI, place the code below in `package.json`. The `nt` key can be anything but the key must be within the `bin` object. The value needs to be the main file which will run the script. 
+To create a CLI, place the code below in `package.json`. The `note` key can be anything but the key must be within the `bin` object. The value needs to be the main file which will run the script. 
 
 ```js
 {
   "bin": {
-    "nt": "./index.js"
+    "note": "./index.js"
   }
 }
 ```
@@ -18,4 +18,4 @@ The main file `./index.js` must contain a hashbang for it to know that it's a No
 
 Then, in the root directory, open a terminal windows and enter `npm link` to create a symbolic link from the global npm directory to your local development directory (the root of this project)
 
-Once it's linked, when you run the `nt` command in the project, it'll execute the `./index.js` file.
+Once it's linked, when you run the `note` command in the project, it'll execute the `./index.js` file.
--- src/command.js
+++ src/command.js
@@ -1,11 +1,57 @@
-import yargs from "yargs"
+import yargs, { argv } from "yargs"
 import { hideBin } from "yargs/helpers"
 
 // hideBin hides the first two arguments of the argv array (note, filename)
 yargs(hideBin(process.argv))
-  // add a command to the cli: note curl <url>, where note is the cli, curl the command, and <url> the arg
-  .command('curl <url>', 'fetch the contents of the URL', () => {}, (argv) => {
-    console.info(argv)
+  /**
+   * Command name followed by any args for it (<> means it's required), command description, yargs.
+   * yargs is a new instance of yargs, but it's limited to this scope, not the one being imported. 
+   * While it's not 100% necessary, it can be useful for formatting things.
+   */
+  .command('new <note>', 'Create a new note', (yargs) => {
+    return yargs.positional('note', {
+      type: 'string',
+      describe: 'the content of the note'
+    })
+  }, (argv) => {
+    console.log(argv.note)
+  })
+  .option('tags', {
+    alias: 't',
+    type: 'string',
+    description: 'tags to add to a note',
+  })
+  .command('all', 'get all notes', () => {}, async (argv) => {
+
+  })
+  .command('find <filter>', 'get matching notes', (yargs) => {
+    return yargs.positional('filter', {
+      type: 'string',
+      describe: 'the search term to filter notes by. will be applied to note.content',
+    }, (argv) => {
+
+    })
+  })
+  .command('remove <id>', 'remove a note by id', (yargs) => {
+    return yargs.positional('id', {
+      type: 'number',
+      describe: 'The id of the note you want to remove',
+    }, async (argv) => {
+
+    })
+  })
+  // when the argument is wrapped within [], such as in web [port], it means it's optional
+  .command('web [port]', 'launch website to see notes', (yargs) => {
+    return yargs.positional('port', {
+      type: 'number',
+      default: 5000, // set the default port to 5000 if none is passed
+      describe: 'port to bind on',
+    })
+  }, async (argv) => {
+
+  })
+  .command('clean', 'remove all notes', () => {}, async (argv) => {
+
   })
   .demandCommand(1)
   .parse()
