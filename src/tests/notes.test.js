3042d0a863a02439b2eb01429bc495f677abac38
Victor Zottmann <zottmann.victor@gmail.com>
2024-01-06 18:31:42 +1100

    add test for `createNote`

--- src/tests/notes.test.js
+++ src/tests/notes.test.js
@@ -1,7 +1,28 @@
-// temporary code
-const add = (num1, num2) => num1 + num2;
+import { jest } from "@jest/globals";
 
-test("add takes two numbers and returns a sum", () => {
-  const result = add(1, 2);
-  expect(result).toBe(3);
+jest.unstable_mockModule("../db.js", () => ({
+  getDB: jest.fn(),
+  saveDB: jest.fn(),
+  insertDB: jest.fn(),
+}));
+
+const { getDB, saveDB, insertDB } = await import("../db.js");
+const { createNote, getAllNotes, removeNote } = await import("../notes.js");
+
+beforeEach(() => {
+  insertDB.mockClear();
+  getDB.mockClear();
+  saveDB.mockClear();
+});
+
+test("createNote inserts data and returns it", async () => {
+  const note = {
+    id: Date.now(),
+    content: "test note",
+    tags: ["hello", "test"],
+  };
+  insertDB.mockResolvedValue(note);
+  const result = await createNote(note.content, note.tags);
+  expect(result.content).toEqual(note.content);
+  expect(result.tags).toEqual(note.tags);
 });
